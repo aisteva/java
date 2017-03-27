77769885430a0f8ee70fb485b0c915b63ab7650b
Aiste <aiste.vaznelyte@gmail.com>
2017-03-27 14:59:07 +0300

    Working on MyBatis relations

---
+++ src/main/java/lt/vu/usecases/cdi/simple/UzsakymasControllerMyBatis.java
@@ -0,0 +1,33 @@
+package lt.vu.usecases.cdi.simple;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import lt.vu.usecases.mybatis.dao.UzsakymasMapper;
+import lt.vu.usecases.mybatis.model.Uzsakymas;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import java.util.List;
+
+
+@Model // tas pats kaip: @Named ir @RequestScoped
+@Slf4j
+public class UzsakymasControllerMyBatis {
+
+
+
+
+    @Getter
+    private Uzsakymas uzsakymas = new Uzsakymas();
+
+    @Inject
+    private UzsakymasMapper uzsakymasMapper;
+
+
+    public List<Uzsakymas> getAllOrders() {
+        return uzsakymasMapper.selectAll();
+    }
+
+
+
+}
