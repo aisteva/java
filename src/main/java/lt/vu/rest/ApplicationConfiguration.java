08733db2cb4cdb5307db8958a0b119b9d3d7845e
Aiste <aiste.vaznelyte@gmail.com>
2017-04-27 08:48:34 +0300

    started rest

---
+++ src/main/java/lt/vu/rest/ApplicationConfiguration.java
@@ -0,0 +1,18 @@
+package lt.vu.rest;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * Created by Aiste on 2017-04-27.
+ */
+@ApplicationPath("/rest")
+public class ApplicationConfiguration extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
